35e6a506dd32bdb025c2b8e97801d58b2048da33
TurtOwl <semenovl632@gmail.com>
2024-09-16 20:12:04 +0300

    Delete atomka directory

--- atomka/src/main/java/com/lexa/atomka/repository/ReactorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.lexa.atomka.repository;
-
-import com.lexa.atomka.model.Reactor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReactorRepository extends JpaRepository<Reactor, Long> {
-}
--- atomka/src/main/java/com/lexa/atomka/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.lexa.atomka.repository;
-
-import com.lexa.atomka.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
